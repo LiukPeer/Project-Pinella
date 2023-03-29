522c2899e3ce67fda4bcedd90798ffe210797d5f
LiukPeer <pera.tellaluca@gmail.com>
2023-03-29 23:08:47 +0200

    Aggiunta descrizione in Carta.java

--- Codice/Carta.java
+++ Codice/Carta.java
@@ -3,7 +3,9 @@ package Codice;
 public class Carta {
     private int valore;
     private String seme;
-
+/* Le carte hanno come valore da 1 a 13, tranne il jolly che vale 0
+   oppure carte da 1 a 12, tranne jolly e pinella
+ */
     public Carta(int value, String sign){
         valore=value;
         seme=sign;
