1ac0c0914ed4a776b160e4c9c02153d1176e3284
LiukPeer <pera.tellaluca@gmail.com>
2023-03-30 17:32:37 +0200

    Hotfix

--- Codice/Mazzo.java
+++ Codice/Mazzo.java
@@ -1,15 +1,15 @@
 package Codice;
 
 public class Mazzo {
-    private int numeroCarte;
-    private int numeroMazzi;
     private DynamicStack deck;
 
     public Mazzo (String gioco){
+        int numeroCarte;
+        int numeroMazzi;
         if (gioco.equalsIgnoreCase("pinella")||gioco.equalsIgnoreCase("machiavelli")||gioco.equalsIgnoreCase("scala")){
             numeroCarte=54;
             numeroMazzi=2;
-            deck = creaMazzo(numeroCarte,numeroMazzi,new String[]{"Picche","Cuori","Fiori","Quadri","Jolly"},13);
+            deck = creaMazzo(numeroCarte,numeroMazzi,new String[]{"Picche","Cuori","Fiori","Quadri"},13);
         }
         else if (gioco.equalsIgnoreCase("scopa")||gioco.equalsIgnoreCase("briscola")){
             numeroCarte=40;
@@ -23,7 +23,16 @@ public class Mazzo {
     }
 
     DynamicStack creaMazzo(int nCarte, int nMazzi, String[] semi, int valoreMassimo){
-        while
+        int count=0;
+        int contS=0;
+        int segno=0;
+        while (count<(nCarte*nMazzi)){
+            new Carta(contS,semi[segno]);
+        }
+    }
+
+    DynamicStack creaMazzoRic(int nCarte, int nMazzi, String[] semi, int valoreMassimo){
+
     }
 
 
