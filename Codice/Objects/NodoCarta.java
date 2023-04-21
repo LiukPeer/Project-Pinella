d442b9879c3c240ee03156966ab38dcea7c07d7c
LiukPeer <pera.tellaluca@gmail.com>
2023-04-21 15:34:31 +0200

    Aggiunto toString a NodoCarta.java

--- Codice/Objects/NodoCarta.java
+++ Codice/Objects/NodoCarta.java
@@ -48,6 +48,58 @@ public class NodoCarta{
             return 1 + length(node.getNext());
         }
     }
+    public static String toString(NodoCarta nodoCarta){
+        String output="";
+        int val=nodoCarta.getValore();
+        if (val==1){
+            output+="Asso";
+        }
+        else if (val==2){
+            output+="Due";
+        }
+        else if (val==3){
+            output+="Tre";
+        }
+        else if (val==4){
+            output+="Quattro";
+        }
+        else if (val==5){
+            output+="Cinque";
+        }
+        else if (val==6){
+            output+="Sei";
+        }
+        else if (val==7&&!nodoCarta.getSeme().equalsIgnoreCase("denari")){
+            output+="Sette";
+        }
+        else if (val==7&&nodoCarta.getSeme().equalsIgnoreCase("denari")){
+            output+="Settebello";
+        }
+        else if (val==8){
+            output+="Otto";
+        }
+        else if (val==9){
+            output+="Nove";
+        }
+        else if (val==10){
+            output+="Dieci";
+        }
+        else if (val==11){
+            output+="Fante";
+        }
+        else if (val==12){
+            output+="Regina";
+        }
+        else if (val==13){
+            output+="Re";
+        }
+        if(val!=0)
+            output+=" di ";
+        if (!(val==7)||!nodoCarta.getSeme().equalsIgnoreCase("denari")){
+            output+=nodoCarta.getSeme();
+        }
+        return output;
+    }
 }
 
 
