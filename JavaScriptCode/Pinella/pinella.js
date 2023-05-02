14d8447529bf600e5603dca4474d9caf149d81e3
Bertez03 <luca.bertetto2@gmail.com>
2023-05-02 18:20:12 +0200

    Ordinamento casuale del mazzo

--- JavaScriptCode/Pinella/pinella.js
+++ JavaScriptCode/Pinella/pinella.js
@@ -10,6 +10,16 @@ function setupMazzo(){
             mazzo.push(new carte(i , semi[j]))
         }
     }
+    shuffleMazzo()
+}
+
+function shuffleMazzo() {
+    for (var i = mazzo.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = mazzo[i];
+        mazzo[i] = mazzo[j];
+        mazzo[j] = temp;
+    }
 }
 
 function pesca(){
@@ -17,7 +27,7 @@ function pesca(){
         let carta = mazzo.pop();
         const myImage = new Image(100, 200);
         myImage.src = "../immagini/playingCards/PNG-cards-1.3/"+carta.src+".png";
-        const grid = document.querySelector(".griglia");
+        const grid = document.querySelector(".mazzo");
         grid.appendChild(myImage);
     }
 }
