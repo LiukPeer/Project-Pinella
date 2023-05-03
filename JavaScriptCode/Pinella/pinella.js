869ac4fe6c2e54ed2bc625a15b985ed95fb1daab
LiukPeer <pera.tellaluca@gmail.com>
2023-05-03 12:44:51 +0200

    ora setupMazzo usa la classe deck e ci sono i jolly

--- JavaScriptCode/Pinella/pinella.js
+++ JavaScriptCode/Pinella/pinella.js
@@ -1,35 +1,19 @@
-
+const valoreMax = 13
 const semi = ["clubs" , "diamonds" , "hearts" , "spades"]
 //const valori = ["ace" , "2" , "3" , "4" , "5" , "6" , "7" , "8" , "9" , "10" , "jack" , "queen" , "king"]
 
-let mazzo = [];
+let mazzoPinella = [];
 
-function setupMazzo(numeroMazzi){
-    for (let nDeck=0; nDeck<numeroMazzi;nDeck++){
-        for(j = 0 ; j < semi.length ; j++){
-            for(i = 1 ; i <= 13 ; i++){
-                mazzo.push(new carte(i , semi[j]))
-            }
-        }
-    }
-    shuffleMazzo()
-}
-
-function shuffleMazzo() {
-    for (let i = mazzo.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        let temp = mazzo[i];
-        mazzo[i] = mazzo[j];
-        mazzo[j] = temp;
-    }
+function setupMazzo(){
+    mazzoPinella= new deck(valoreMax,semi,true,2).getMazzo()
 }
 
 function pesca(){
-    if(mazzo.length > 0){
-        let carta = mazzo.pop();
+    if(mazzoPinella.length > 0){
+        let carta = mazzoPinella.pop();
         const myImage = new Image(75, 150);
         myImage.src = "../immagini/PNG-cards-1.3/"+carta.src+".png";
-        const grid = document.querySelector(".mazzo");
+        const grid = document.querySelector(".mazzoPinella");
         grid.appendChild(myImage);
     }
 }
--- JavaScriptCode/playingCards/deck.js
+++ JavaScriptCode/playingCards/deck.js
@@ -9,19 +9,23 @@ class deck{
                 }
             }
             if (booleanJoker===true) {
-                this.mazzo.push(new joker(0, null, 'red'))
-                this.mazzo.push(new joker(0, null, 'black'))
+                this.mazzo.push(new joker(0, '', 'red'))
+                this.mazzo.push(new joker(0, '', 'black'))
             }
         }
         this.shuffleMazzo()
     }
 
     shuffleMazzo() {
-        for (let i = this.mazzo.length - 1; i > 0; i--) {
-            let j = Math.floor(Math.random() * (i + 1));
-            let temp = this.mazzo[i];
-            this.mazzo[i] = this.mazzo[j];
-            this.mazzo[j] = temp;
+        for (let count = this.mazzo.length - 1; count > 0; count--) {
+            let random = Math.floor(Math.random() * (count + 1));
+            let temp = this.mazzo[count];
+            this.mazzo[count] = this.mazzo[random];
+            this.mazzo[random] = temp;
         }
+    }
+
+    getMazzo(){
+        return this.mazzo
     }
 }
