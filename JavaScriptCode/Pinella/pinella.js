3c2e028a87063ddeeafaf9c3f82258e358d1d74f
LiukPeer <pera.tellaluca@gmail.com>
2023-05-03 11:14:38 +0200

    bug fix

--- JavaScriptCode/Pinella/pinella.js
+++ JavaScriptCode/Pinella/pinella.js
@@ -2,7 +2,7 @@
 const semi = ["clubs" , "diamonds" , "hearts" , "spades"]
 //const valori = ["ace" , "2" , "3" , "4" , "5" , "6" , "7" , "8" , "9" , "10" , "jack" , "queen" , "king"]
 
-var mazzo = []
+let mazzo = [];
 
 function setupMazzo(numeroMazzi){
     for (let nDeck=0; nDeck<numeroMazzi;nDeck++){
@@ -16,9 +16,9 @@ function setupMazzo(numeroMazzi){
 }
 
 function shuffleMazzo() {
-    for (var i = mazzo.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = mazzo[i];
+    for (let i = mazzo.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = mazzo[i];
         mazzo[i] = mazzo[j];
         mazzo[j] = temp;
     }
@@ -27,8 +27,8 @@ function shuffleMazzo() {
 function pesca(){
     if(mazzo.length > 0){
         let carta = mazzo.pop();
-        const myImage = new Image(100, 200);
-        myImage.src = "../immagini/playingCards/PNG-cards-1.3/"+carta.src+".png";
+        const myImage = new Image(75, 150);
+        myImage.src = "../immagini/PNG-cards-1.3/"+carta.src+".png";
         const grid = document.querySelector(".mazzo");
         grid.appendChild(myImage);
     }
