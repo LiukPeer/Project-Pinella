22b5eea26ff4606b561983fa3b53c6dc55701eaa
Bertez03 <luca.bertetto2@gmail.com>
2023-05-05 19:48:47 +0200

    implementazione dello scarto

--- JavaScriptCode/Pinella/js/pinella.js
+++ JavaScriptCode/Pinella/js/pinella.js
@@ -5,10 +5,15 @@ let giocatori =[]
 const nCarte = 13
 const semi = ["clubs" , "diamonds" , "hearts" , "spades"]
 
+function caricaGiocatori(){
+    giocatori.push(new giocatore(1 , "prova"))
+}
+
+
 function distribuisci(){
     for(let i = 0; i < nCarte; i++){
         for(let j = 0 ; j < giocatori.length ; j++){
-            giocatori[j].mano.push(mazzo.pop())
+            pescaMazzo(giocatori[j]);
         }
     }
 }
@@ -17,21 +22,33 @@ function setupMazzo(){
     mazzo = new deck(13 , semi ,true, 2, true).getMazzo()
 }
 
-function pescaMazzo(){
+function pescaMazzo(giocatore){
     let carta = mazzo.pop();
-    
+    giocatore.mano.push(carta);
+
     let myImage = new Image(50, 75);
     myImage.src = "../immagini/PNG-cards-1.3/"+carta.src+".png";
-    let mano = document.querySelector(".giocatore");
-    mano.appendChild(myImage);
+    myImage.id = "image" + giocatore.mano.length; 
 
-    console.log(mazzo.length)
+    document.querySelector(".giocatore").appendChild(myImage);
+    
+    myImage.addEventListener("dragstart" ,   function drag(ev) {
+        ev.dataTransfer.setData("image", ev.target.id);
+      });
 }
-
-function scarto(){
-
-}
+// Questa parte gestisce lo carto
+function allowDrop(ev) {
+    ev.preventDefault();
+  }
+  
+  function scarto(ev) {
+    ev.preventDefault();
+    let data = ev.dataTransfer.getData("image");
+    document.querySelector(".scarto").appendChild(document.getElementById(data));
+  }
 
 function pescaScarto() {
 
 }
+
+
