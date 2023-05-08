dadbf9192809901cfd66ccce37ff9157d1bb1183
Luis Andrè <luisbarriosaraujo@outlook.it>
2023-05-08 12:12:57 +0200

    Creazione users
    Co-Authored-By: Bertez03 <117892809+Bertez03@users.noreply.github.com>

--- JavaScriptCode/Pinella/js/connessione.js
+++ JavaScriptCode/Pinella/js/connessione.js
@@ -16,37 +16,3 @@ const firebaseConfig = {
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig)
-
-(function () {
-  firebase.auth().onAuthStateChanged((user) => {
-    console.log(user);
-    if (user) {
-      //You're logged in!
-      playerId = user.uid;
-      playerRef = firebase.database().ref(`players/${playerId}`);
-
-      const name = createName();
-      playerNameInput.value = name;
-
-      playerRef.set({
-        id: playerId,
-        name
-      });
-
-      //Remove me from Firebase when I diconnect
-      playerRef.onDisconnect().remove();
-
-      //Begin the game now that we are signed in
-      initGame();
-    } else {
-      //You're logged out.
-    }
-  });
-  firebase.auth().signInAnonymously().catch((error) => {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // ...
-    console.log(errorCode, errorMessage);
-  });
-
-});
